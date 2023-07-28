7d4dae0abfcc229044a51e96ae0650531e86a8a9
Aleksei Semochkin <aleksioprime@gmail.com>
2023-07-28 14:12:47 +0300

    part 4

--- demo/src/demo1/DemoCounter.vue
+++ demo/src/demo1/DemoCounter.vue
@@ -1,18 +1,20 @@
 <template>
   <demo-container>
     <div class="counter">
-        <button @click="decrement"> - </button>
+      <counter-button @action="decrement">-</counter-button>
 
-        <span> Current value: {{ count }} </span>
+      <counter-value :value="count" />
 
-        <button @click="increment"> + </button>
+      <counter-button @action="increment">+</counter-button>
     </div>
   </demo-container>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import CounterButton from "./CounterButton.vue";
+import CounterValue from "./CounterValue.vue";
 import DemoContainer from '../components/DemoContainer.vue';
+import { ref } from 'vue'
 
 const count = ref(0)
 
@@ -31,21 +33,5 @@ const increment = () => {
 .counter {
   display: flex;
   align-items: center;
-
-  button {
-    margin: 0 10px;
-    font-size: 1.5rem;
-    width: 2rem;
-    height: 2rem;
-    border-radius: 50%;
-    border: none;
-    background: #dd0000;
-    color: white;
-    cursor: pointer;
-
-    &:hover {
-      background: #ff0000;
-    }
-  }
 }
 </style>
--- demo/src/routes.js
+++ demo/src/routes.js
@@ -1,7 +1,12 @@
 export default [
     {
-      name: 'Counter Base',
+      name: 'Counter Base 0',
       path: '/counter-0',
       component: () => import("./demo0/DemoCounter.vue"),
+    },
+    {
+      name: 'Counter Base 1',
+      path: '/counter-1',
+      component: () => import("./demo1/DemoCounter.vue"),
     },
   ];
